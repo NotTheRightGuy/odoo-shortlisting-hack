ce2f90e4ee96ec131ddbfef7214d71ef92c93dee
Devvrat Singh Rathod <rathoddevvratsingh777@gmail.com>
2024-06-29 19:32:49 +0530

    up todate

--- app/police/crimehistory/page.tsx
+++ app/police/crimehistory/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import * as React from 'react';
-// import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 import { useState, useEffect } from 'react';
 
 
@@ -8,8 +8,6 @@ export default function CrimeHistory() {
     
     // const [crimeHistory, setCrimeHistory] = useState([]);
     const [crimeHistory,setCrimeHistory]=useState<any[]>([])
-    // const [docs, setDocs] = useState(documents || []);
-
 
     useEffect(() => {
         fetch('/api/crimeCRUD/fetchCrimeHistory')
@@ -22,39 +20,37 @@ export default function CrimeHistory() {
     }, []);
 
 
-    const columns  = [
-        { field: 'id', headerName: 'C_ID', width: 150 },
-        { field: 'C_DATE', headerName: 'C_DATE', width: 150 },
-        { field: 'C_DESC', headerName: 'C_DESC', width: 150 },
-        { field: 'C_LATITUDE', headerName: 'C_LATITUDE', width: 150 },
-        { field: 'C_LONGITUDE', headerName: 'C_LONGITUDE', width: 150 },
-        { field: 'C_NAME', headerName: 'C_NAME', width: 150 },
-        { field: 'C_STATUS', headerName: 'C_STATUS', width: 150 },
-        { field: 'C_TIME', headerName: 'C_TIME', width: 150 },
-        { field: 'C_TYPE', headerName: 'C_TYPE', width: 150 }
-    ];
+    // const columns  = [
+    //     { field: 'id', headerName: 'C_ID', width: 150 },
+    //     { field: 'C_DATE', headerName: 'C_DATE', width: 150 },
+    //     { field: 'C_DESC', headerName: 'C_DESC', width: 150 },
+    //     { field: 'C_LATITUDE', headerName: 'C_LATITUDE', width: 150 },
+    //     { field: 'C_LONGITUDE', headerName: 'C_LONGITUDE', width: 150 },
+    //     { field: 'C_NAME', headerName: 'C_NAME', width: 150 },
+    //     { field: 'C_STATUS', headerName: 'C_STATUS', width: 150 },
+    //     { field: 'C_TIME', headerName: 'C_TIME', width: 150 },
+    //     { field: 'C_TYPE', headerName: 'C_TYPE', width: 150 }
+    // ];
 
-    const rows =
-    crimeHistory.map((crime)=>{
-            return(
-                {
-                    id:crime.C_ID,
-                    C_DATE:crime.C_DATE,
-                    C_DESC:crime.C_DESC,
-                    C_LATITUDE:crime.C_LATITUDE,
-                    C_LONGITUDE:crime.C_LONGITUDE,
-                    C_NAME:crime.C_NAME,
-                    C_STATUS:crime.C_STATUS,
-                    C_TIME:crime.C_TIME,
-                    C_TYPE:crime.C_TYPE
-                }
-            )
-        })
+    // const rows =
+    // crimeHistory.map((crime)=>{
+    //         return(
+    //             {
+    //                 id:crime.C_ID,
+    //                 C_DATE:crime.C_DATE,
+    //                 C_DESC:crime.C_DESC,
+    //                 C_LATITUDE:crime.C_LATITUDE,
+    //                 C_LONGITUDE:crime.C_LONGITUDE,
+    //                 C_NAME:crime.C_NAME,
+    //                 C_STATUS:crime.C_STATUS,
+    //                 C_TIME:crime.C_TIME,
+    //                 C_TYPE:crime.C_TYPE
+    //             }
+    //         )
+    //     })
     return(
         <div>
-            Checking
-            {/* <DataGrid rows={rows} columns={columns} /> */}
-
+            "Checking"
         </div>
     )
 }
--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -2,35 +2,50 @@
 import React from "react";
 import WatchDogsIcon from "./WatchDogsIcon";
 import Link from "next/link";
+import { UserButton, useUser } from "@clerk/nextjs";
 
 function Navbar() {
-  return (
-    <header className="absolute right-0 top-0 flex h-fit z-10 w-full items-center justify-between px-20 py-5">
-      <div className="">
-        <WatchDogsIcon width={250} height={60} />
-      </div>
-      <nav className="flex items-center gap-14">
-        <Link href={"/"} className="text-base font-normal text-gray-600">
-          Reports
-        </Link>
-        <Link href={"/"} className="text-base font-normal text-gray-600">
-          Blogs
-        </Link>
-        <Link href={"/"} className="text-base font-normal text-gray-600">
-          Cases
-        </Link>
-        <Link href={"/"} className="text-base font-normal text-gray-600">
-          Security
-        </Link>
-        <Link
-          href={"/auth/sign-up"}
-          className="rounded-3xl bg-blue-600 px-7 py-4 text-base font-bold text-white"
-        >
-          Create Account
-        </Link>
+    const { isLoaded, isSignedIn, user } = useUser();
+
+    return (
+        <header className="absolute right-0 top-0 flex h-fit z-10 w-full items-center justify-between px-20 py-5">
+            <div className="">
+                <WatchDogsIcon width={250} height={60} />
+            </div>
+            <nav className="flex items-center gap-14">
+
+                <Link href={"/"} className="text-base font-normal text-gray-600">
+                    Reports
+                </Link>
+                {
+                    isSignedIn ? (
+                        <Link href={"/user/dashboard"} className="text-base font-normal text-gray-600">
+                            Dashboard
+                        </Link>
+                    ) : null
+                }
+                {/* <Link href={"/"} className="text-base font-normal text-gray-600">
+                    Dashboard
+                </Link> */}
+                <Link href={"/"} className="text-base font-normal text-gray-600">
+                    Cases
+                </Link>
+                <Link href={"/"} className="text-base font-normal text-gray-600">
+                    Security
+                </Link>
+                {isSignedIn ? (
+                    <UserButton />) :
+                    <Link
+                        href={"/auth/sign-up"}
+                        className="rounded-3xl bg-blue-600 px-7 py-4 text-base font-bold text-white"
+                    >
+                        "Create Account"
+                    </Link>
+                }
+
       </nav>
-    </header>
-  );
+        </header>
+    );
 }
 
 export default Navbar;
