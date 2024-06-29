c5bd65f7c4e2d6ac29f1076851a2a607731fc248
NotTheRightGuy <janmejaychatterjee@gmail.com>
2024-06-29 15:21:10 +0530

    chore: Remove unused code and dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,5 @@
-"use client";
-
-import React from "react";
-import { supabase } from "@/db";
+import Landing from "@/components/page/Landing";
 
 export default function Home() {
-  function handleInsert(payload: any) {
-    console.log("Change Received!", payload);
-  }
-
-  React.useEffect(() => {
-    supabase
-      .channel("CRIME_DETAILS")
-      .on(
-        "postgres_changes",
-        {
-          event: "INSERT",
-          schema: "public",
-          table: "CRIME_DETAILS",
-        },
-        handleInsert
-      )
-      .subscribe();
-  }, []);
-  return <div>Landing</div>;
+  return <Landing />;
 }
--- components/page/Landing.tsx
+++ components/page/Landing.tsx
@@ -2,10 +2,10 @@ import Map from "../Map";
 import Section1 from "./Section1";
 import Section5 from "./Section5";
 export default function Landing() {
-    return (
-        <div className="h-max w-screen">
-            <Section1 />
-            <Section5 />
-        </div>
-    );
+  return (
+    <div className="h-max w-screen">
+      <Section1 />
+      <Section5 />
+    </div>
+  );
 }
