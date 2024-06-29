26a546811d78c71c0422e648d0c055d327cce03f
Vedant Panchal <VEDANTPANCHAL.ict22@adaniuni.ac.in>
2024-06-29 13:22:27 +0530

    Refactor landing page component and update styling

--- components/page/Section1.tsx
+++ components/page/Section1.tsx
@@ -1,21 +1,41 @@
-import React from 'react'
-import Navbar from '../ui/Navbar'
-import Link from 'next/link'
+import React from "react";
+import Navbar from "../ui/Navbar";
+import Link from "next/link";
 
 function Section1() {
   return (
-    <div className='relative'>
-        <Navbar />
-        <div className='w-screen h-screen flex flex-col items-start gap-5 justify-center px-20'>
-            <h1 className='font-extrabold tracking-tight text-5xl'>
-                Empowering Communities,<br />Enhancing <span className='text-blue-700'>Security</span>
-            </h1>
-            <p className='text-lg text-gray-600 leading-9'>Empowering Communities to <span className='bg-pink-500 px-4 py-1.5 text-slate-50 rounded-xl'>Report</span> Incidents, Enabling <br />
-             Switch <span className='bg-blue-600 px-4 py-1.5  text-slate-50 rounded-xl'>Response</span> ,and Facilitating Effective <span className='bg-orange-500 px-4 py-1.5 text-slate-50 rounded-xl'>Resolution</span></p>
-             <Link href={'/'} className='text-base bg-slate-900 text-slate-50 rounded-xl px-4 py-3 font-bold'>Report Incident</Link>
-        </div>
+    <div className="relative">
+      <Navbar />
+      <div className="flex h-screen w-screen flex-col items-start justify-center gap-5 px-20">
+        <h1 className="text-5xl font-extrabold tracking-tight">
+          Empowering Communities,
+          <br />
+          Enhancing <span className="text-blue-700">Security</span>
+        </h1>
+        <p className="text-lg leading-9 text-gray-600">
+          Empowering Communities to{" "}
+          <span className="rounded-xl bg-pink-500 px-4 py-1.5 text-slate-50">
+            Report
+          </span>{" "}
+          Incidents, Enabling <br />
+          Switch{" "}
+          <span className="rounded-xl bg-blue-600 px-4 py-1.5 text-slate-50">
+            Response
+          </span>{" "}
+          ,and Facilitating Effective{" "}
+          <span className="rounded-xl bg-orange-500 px-4 py-1.5 text-slate-50">
+            Resolution
+          </span>
+        </p>
+        <Link
+          href={"/"}
+          className="rounded-xl bg-slate-900 px-4 py-3 text-base font-bold text-slate-50"
+        >
+          Report Incident
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Section1
+export default Section1;
--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,23 +1,36 @@
 "use client";
-import React from 'react'
-import WatchDogsIcon from './WatchDogsIcon'
-import Link from 'next/link'
+import React from "react";
+import WatchDogsIcon from "./WatchDogsIcon";
+import Link from "next/link";
 
 function Navbar() {
   return (
-    <header className='flex items-center justify-between absolute top-0 right-0 w-full h-fit px-20 py-5'>
-        <div className=''>
-            <WatchDogsIcon width={250} height={60} />
-        </div>
-        <nav className='flex items-center gap-14'>
-            <Link href={'/'} className='text-base font-normal text-gray-600'>Reports</Link>
-            <Link href={'/'} className='text-base font-normal text-gray-600'>Blogs</Link>
-            <Link href={'/'} className='text-base font-normal text-gray-600'>Cases</Link>
-            <Link href={'/'} className='text-base font-normal text-gray-600'>Security</Link>
-            <Link href={'/'} className='text-base text-white bg-blue-600 px-7 py-4 font-bold rounded-3xl'>Create Account</Link>
-        </nav>
+    <header className="absolute right-0 top-0 flex h-fit w-full items-center justify-between px-20 py-5">
+      <div className="">
+        <WatchDogsIcon width={250} height={60} />
+      </div>
+      <nav className="flex items-center gap-14">
+        <Link href={"/"} className="text-base font-normal text-gray-600">
+          Reports
+        </Link>
+        <Link href={"/"} className="text-base font-normal text-gray-600">
+          Blogs
+        </Link>
+        <Link href={"/"} className="text-base font-normal text-gray-600">
+          Cases
+        </Link>
+        <Link href={"/"} className="text-base font-normal text-gray-600">
+          Security
+        </Link>
+        <Link
+          href={"/"}
+          className="rounded-3xl bg-blue-600 px-7 py-4 text-base font-bold text-white"
+        >
+          Create Account
+        </Link>
+      </nav>
     </header>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
