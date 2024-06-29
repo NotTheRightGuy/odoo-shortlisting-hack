bdc90a48ce9fd10527129225e258477ec5ee74ec
Devvrat Singh Rathod <rathoddevvratsingh777@gmail.com>
2024-06-29 13:27:38 +0530

    added icons commit 2

--- components/page/section5.tsx
+++ components/page/section5.tsx
@@ -1,14 +1,14 @@
 import logo from "@/assets/logo.png";
 import Image from "next/image";
-// import 
+// import
 
 export default function Section5() {
-    return (
-        <div className="flex">
-            <div>
-                <Image src={logo} alt="logo" />
-                <p> Transforming Security </p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex">
+      <div>
+        <Image src={logo} alt="logo" />
+        <p> Transforming Security </p>
+      </div>
+    </div>
+  );
 }
