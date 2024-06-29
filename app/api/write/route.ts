13ac38f92e95c3ce52e809ff95937accea7d2976
Devvrat Singh Rathod <rathoddevvratsingh777@gmail.com>
2024-06-29 12:13:44 +0530

    conflict resolved in packagejson

--- app/api/write/route.ts
+++ app/api/write/route.ts
@@ -1,30 +1,25 @@
-import { QueryResult, QueryData, QueryError } from '@supabase/supabase-js'
-import { NextResponse } from 'next/server';
+import { QueryResult, QueryData, QueryError } from "@supabase/supabase-js";
+import { NextResponse } from "next/server";
 
-import { createClient } from '@supabase/supabase-js'
-import { Database } from '@/database.types'
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "@/database.types";
 
 const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
-)
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
+);
 
+export async function GET(request: any, response: any) {
+  try {
+    const { data, error } = await supabase.from("USER_DETAILS").select();
 
-export async function GET(request:any, response:any) {
-    try {
-        const { data, error } = await supabase
-        .from('USER_DETAILS')
-        .select()
-    
-        if (error) {
-            console.error(error)
-            return
-            }
-        console.log(data)
-        return NextResponse.json(data);
-
+    if (error) {
+      console.error(error);
+      return;
     }
-    catch (err) {
-        return NextResponse.error();
-    }
+    console.log(data);
+    return NextResponse.json(data);
+  } catch (err) {
+    return NextResponse.error();
+  }
 }
