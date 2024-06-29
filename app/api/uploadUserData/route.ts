81b5d51ca1bdd39cb9f4681377e41ae1a87cf8c6
Devvrat Singh Rathod <rathoddevvratsingh777@gmail.com>
2024-06-29 15:28:32 +0530

    chore: Refactor uploadUserData route to improve code readability

--- app/api/uploadUserData/route.ts
+++ app/api/uploadUserData/route.ts
@@ -8,16 +8,14 @@ const supabase = createClient<Database>(
 );
 
 export async function GET(request: any, response: any) {
-  const { data, error } = await supabase
-    .from("USER_DETAILS")
-    .insert({
-      U_ID: request.query.U_ID,
-      HOUSE_NO: request.query.HOUSE_NO,
-      SOCIETY_NAME: request.query.SOCIETY_NAME,
-      CITY: request.query.CITY,
-      STATE: request.query.STATE,
-      PINCODE: request.query.PINCODE,
-    });
+  const { data, error } = await supabase.from("USER_DETAILS").insert({
+    U_ID: request.query.U_ID,
+    HOUSE_NO: request.query.HOUSE_NO,
+    SOCIETY_NAME: request.query.SOCIETY_NAME,
+    CITY: request.query.CITY,
+    STATE: request.query.STATE,
+    PINCODE: request.query.PINCODE,
+  });
 
   try {
     return NextResponse.json(data);
