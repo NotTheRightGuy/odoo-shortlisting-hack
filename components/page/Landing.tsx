9eedaf80d34678d8710918698c09b43605fd0ea6
Devvrat Singh Rathod <rathoddevvratsingh777@gmail.com>
2024-06-29 15:06:16 +0530

    completed section5

--- components/page/Landing.tsx
+++ components/page/Landing.tsx
@@ -1,9 +1,11 @@
 import Map from "../Map";
 import Section1 from "./Section1";
+import Section5 from "./Section5";
 export default function Landing() {
-  return (
-    <div className="h-max w-screen">
-      <Section1 />
-    </div>
-  );
+    return (
+        <div className="h-max w-screen">
+            <Section1 />
+            <Section5 />
+        </div>
+    );
 }
