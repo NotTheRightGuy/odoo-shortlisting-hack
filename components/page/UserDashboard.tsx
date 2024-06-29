02e9a544e6dc3f73459d1dc23ecfc71ac2028e0d
NotTheRightGuy <janmejaychatterjee@gmail.com>
2024-06-29 19:17:53 +0530

    chore: Optimize imports and update styling in Select component

--- components/page/UserDashboard.tsx
+++ components/page/UserDashboard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Map from "../Map";
 import WatchDogsIcon from "../ui/WatchDogsIcon";
@@ -20,7 +21,15 @@ import {
 import { Textarea } from "@/components/ui/textarea";
 import { Input } from "@/components/ui/input";
 
+import { supabase } from "@/db";
+
 export default function UserDashboard() {
+  const [crimeType, setCrimeType] = React.useState("");
+  const [status, setStatus] = React.useState("");
+  const [description, setDescription] = React.useState("");
+  const [name, setName] = React.useState("anonymous");
+  const [pos, setPos] = React.useState({ lat: 0, lng: 0 });
+
   return (
     <>
       <main className="h-screen">
@@ -37,12 +46,10 @@ export default function UserDashboard() {
         <div className="flex border-t-2 h-[85vh]">
           <div className="px-4 py-6 flex-1 ">
             <div className="w-full flex justify-between items-center py-2">
-              <p className="font-bold text-2xl">Home</p>
+              <p className="font-bold text-2xl px-2">Home</p>
               <Sheet>
-                <SheetTrigger>
-                  <Button className="bg-blue-600 hover:bg-blue-700 text-white rounded-full">
-                    Report Incident
-                  </Button>
+                <SheetTrigger className="bg-blue-600 hover:bg-blue-700 text-white rounded-full px-4 py-2">
+                  Report Incident
                 </SheetTrigger>
                 <SheetContent>
                   <SheetHeader>
@@ -53,9 +60,37 @@ export default function UserDashboard() {
                     </SheetDescription>
                   </SheetHeader>
                   <form
-                    onSubmit={(e) => {
+                    onSubmit={async (e) => {
                       e.preventDefault();
-                      // Add your form submission logic here
+                      // Get current location from the browser
+                      navigator.geolocation.getCurrentPosition(
+                        async (position) => {
+                          setPos({
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude,
+                          });
+
+                          const { data, error } = await supabase
+                            .from("CRIME_DETAILS")
+                            .insert([
+                              {
+                                C_TYPE: crimeType,
+                                C_LATITUDE: position.coords.latitude,
+                                C_LONGITUDE: position.coords.longitude,
+                                C_SEVERITY: status,
+                                C_DESC: description,
+                                C_NAME: name,
+                              },
+                            ]);
+                          if (error) {
+                            alert(
+                              "An error occurred while reporting the incident"
+                            );
+                          } else {
+                            alert("Incident reported successfully");
+                          }
+                        }
+                      );
                     }}
                     className="space-y-4 mt-4"
                   >
@@ -66,7 +101,10 @@ export default function UserDashboard() {
                       >
                         Type Of Crime
                       </label>
-                      <Select name="crimeType">
+                      <Select
+                        name="crimeType"
+                        onValueChange={(value) => setCrimeType(value)}
+                      >
                         <SelectTrigger>
                           <SelectValue placeholder="Select a crime type" />
                         </SelectTrigger>
@@ -86,19 +124,37 @@ export default function UserDashboard() {
                       >
                         Status
                       </label>
-                      <Select name="status">
+                      <Select
+                        name="Severity"
+                        onValueChange={(val) => {
+                          setStatus(val);
+                        }}
+                      >
                         <SelectTrigger>
-                          <SelectValue placeholder="Select status" />
+                          <SelectValue placeholder="Severity" />
                         </SelectTrigger>
                         <SelectContent>
-                          <SelectItem value="reported">Reported</SelectItem>
-                          <SelectItem value="under-investigation">
-                            Under Investigation
-                          </SelectItem>
-                          <SelectItem value="resolved">Resolved</SelectItem>
-                          <SelectItem value="closed">Closed</SelectItem>
+                          <SelectItem value="low">Low</SelectItem>
+                          <SelectItem value="medium">Moderate</SelectItem>
+                          <SelectItem value="high">Critical</SelectItem>
                         </SelectContent>
                       </Select>
+                    </div>
+                    <div>
+                      <label
+                        htmlFor="name"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Crime Topic in short
+                      </label>
+                      <Input
+                        id="name"
+                        name="name"
+                        placeholder="Enter the crime topic in short"
+                        onChange={(e) => {
+                          setName(e.target.value);
+                        }}
+                      />
                     </div>
                     <div>
                       <label
@@ -112,22 +168,16 @@ export default function UserDashboard() {
                         name="description"
                         placeholder="Describe the crime"
                         rows={4}
+                        onChange={(e) => {
+                          setDescription(e.target.value);
+                        }}
                       />
                     </div>
-                    <div>
-                      <label
-                        htmlFor="name"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Name
-                      </label>
-                      <Input
-                        id="name"
-                        name="name"
-                        placeholder="Enter your name"
-                      />
-                    </div>
-                    <Button type="submit" className="mt-4">
+
+                    <Button
+                      type="submit"
+                      className="mt-4 bg-green-500 text-white rounded-sm hover:bg-green-600"
+                    >
                       Submit
                     </Button>
                   </form>
