20c87d28de89b952c21f58b5a81e39ae07358b3a
Devvrat Singh Rathod <rathoddevvratsingh777@gmail.com>
2024-06-29 15:28:18 +0530

    Commit no idk

--- app/api/uploadUserData/route.ts
+++ app/api/uploadUserData/route.ts
@@ -1,22 +1,27 @@
-import { createClient } from '@supabase/supabase-js'
-import { Database } from '@/database.types'
-import { NextResponse } from 'next/server';
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "@/database.types";
+import { NextResponse } from "next/server";
 
 const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
-)
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
+);
 
-export async function GET(request:any, response:any) {
-    
-    const { data, error } = await supabase
-    .from('USER_DETAILS')
-    .insert({U_ID : request.query.U_ID,  HOUSE_NO : request.query.HOUSE_NO, SOCIETY_NAME : request.query.SOCIETY_NAME, CITY : request.query.CITY, STATE : request.query.STATE, PINCODE : request.query.PINCODE})
+export async function GET(request: any, response: any) {
+  const { data, error } = await supabase
+    .from("USER_DETAILS")
+    .insert({
+      U_ID: request.query.U_ID,
+      HOUSE_NO: request.query.HOUSE_NO,
+      SOCIETY_NAME: request.query.SOCIETY_NAME,
+      CITY: request.query.CITY,
+      STATE: request.query.STATE,
+      PINCODE: request.query.PINCODE,
+    });
 
-    try {
-        return NextResponse.json(data);
-    }
-    catch (err) {
-        return NextResponse.error();
-    }
+  try {
+    return NextResponse.json(data);
+  } catch (err) {
+    return NextResponse.error();
+  }
 }
